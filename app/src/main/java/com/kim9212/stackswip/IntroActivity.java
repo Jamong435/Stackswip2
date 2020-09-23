9a9b0eae3fae46c3722c080e99b9c8f19b2351a5
KYH <toki666@naver.com>
2020-09-23 10:00:15 +0900

    commit

--- app/src/main/java/com/kim9212/stackswip/IntroActivity.java
+++ app/src/main/java/com/kim9212/stackswip/IntroActivity.java
@@ -1,7 +1,11 @@
 package com.kim9212.stackswip;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IntroActivity extends AppCompatActivity {
@@ -10,5 +14,18 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        handler.sendEmptyMessageDelayed(0,1500);
+
+
+
     }
+    Handler handler= new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            Intent intent= new Intent(IntroActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
+    };
 }
